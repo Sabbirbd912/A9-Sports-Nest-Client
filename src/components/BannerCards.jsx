1cb7cb00c3609024b191418763cf933b066e3333
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-07-07 15:59:58 +0600

    popular facilities upsated

--- src/components/BannerCards.jsx
+++ src/components/BannerCards.jsx
@@ -20,7 +20,8 @@ const BannerCards = async () => {
   const facilities = await getFacilities();
 
   return (
-    <div id="featured-facilities" className="max-w-7xl mx-auto px-4 py-12">
+    <div id="featured-facilities" className="max-w-7xl mx-auto px-4 py-20">
+      <h2 className="text-5xl text-neutral-900 font-bold text-center pb-10">Popular <span className="text-lime-500">Facilities</span> </h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-6">
         {facilities.slice(0, 6).map((facility) => (
           <FacilityCard key={facility._id} facility={facility} />
